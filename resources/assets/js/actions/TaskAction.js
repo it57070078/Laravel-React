0517f6b76410d4bf6d5d67eb1ea13790edf199cf
Patcharapon <msophon38@gmail.com>
2017-06-15 15:09:19 +0700

    Test Flux Don't Compelete wait Edit

---
+++ resources/assets/js/actions/TaskAction.js
@@ -0,0 +1,15 @@
+// import TaskDispatcher from './dispatcher/TaskDispatcher.js';
+// import TaskConstants from './constants/TaskConstants.js';
+
+// class TaskActions {
+//     addTask(e){
+//         TaskDispatcher.dispatch({
+//             actionType: TaskConstants.addTask,e: taskData,
+//         });
+//     }
+//     deleteTask(e){
+
+//     }
+// }
+
+// module.exports = TaskActions;
---
+++ resources/assets/js/dispatcher/TaskDispatcher.js
@@ -0,0 +1,5 @@
+// import Dispatcher from 'flux';
+
+// new TaskDispatcher();
+
+// module.exports= Dispatcher;
